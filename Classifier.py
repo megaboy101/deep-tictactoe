dad708b7f3c3f971c48571343a617f7827fd72c3
Jacob Bleser <bleserjacob@gmail.com>
2021-02-08 18:34:09 -0500

    Added Multi-Layer Perceptron Model

--- Classifier.py
+++ Classifier.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn import neighbors, svm, model_selection, metrics
+from sklearn import neighbors, svm, neural_network, model_selection, metrics
 
 class Classifier:
   def __init__(self):
@@ -14,6 +14,10 @@ class Classifier:
     print("LINEAR SVM")
     print("===================================")
     self.linear_svc(X, y)
+    print("")
+    print("MULTILAYER PERCEPTRON")
+    print("===================================")
+    self.perceptron(X, y)
   
   def confusion_matrix(self, matrix):
     head_template = "{0:10}{1:13}{2:13}"
@@ -49,6 +53,21 @@ class Classifier:
     X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
 
     classifier = svm.SVC(kernel="linear", C=1)
+    classifier.fit(X_train, y_train)
+
+    y_pred = classifier.predict(X_test)
+
+    matrix = [ row / np.linalg.norm(row) for row in metrics.confusion_matrix(y_test, y_pred) ]
+    self.confusion_matrix(matrix)
+    print("")
+    print("Model Accuracy: %0.6f" % metrics.accuracy_score(y_test, y_pred))
+
+  def perceptron(self, X, y):
+    # Split data into training and testing sets 80/20 at random
+    # Shuffle dataset before splitting
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
+
+    classifier = neural_network.MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(1, 1), random_state=1)
     classifier.fit(X_train, y_train)
 
     y_pred = classifier.predict(X_test)
